adb544c87e3c0b92a183b219972f719bbddc66df
djakimiuk <63128911+djakimiuk@users.noreply.github.com>
2023-06-18 19:35:21 +0200

    neighbours counter func implemented

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,16 +3,24 @@ import { useEffect, useState } from "react";
 import board from "../ExamInput";
 
 function Grid() {
-  
+  const [gridToDisplay, setGrid] = useState(board);
+  const clickSquareValueHandler = (row, col) => {
+    const gridCopy = [...gridToDisplay];
+    gridCopy[row][col] === "0"
+      ? (gridCopy[row][col] = "1")
+      : (gridCopy[row][col] = "0");
+    setGrid(gridCopy);
+  };
   const renderGrid = () => {
     const grid = [];
-    for (let row = 0; row < gridToDisplay.length; row++) {
+    for (let row = 0; row < 20; row++) {
       grid.push([]);
-      for (let col = 0; col < gridToDisplay[0].length; col++) {
+      for (let col = 0; col < 20; col++) {
         grid[row].push(
           <GridSquare
             key={`${col}${row}`}
             color={`${gridToDisplay[row][col]}`}
+            onClick={() => clickSquareValueHandler(row, col)}
           />
         );
       }
@@ -20,6 +28,23 @@ function Grid() {
     return grid;
   };
 
+  const countSquareNeighbours = (squarePosition) => {
+    const vectors = [-1, 0, 1];
+    let squareRow = squarePosition[0];
+    let squareCol = squarePosition[1];
+    let neighboursCount = gridToDisplay[squareRow][squareCol] === "1" ? -1 : 0;
+    for (let i = 0; i < vectors.length; i++) {
+      for (let j = 0; j < vectors.length; j++) {
+        if (
+          gridToDisplay[squareRow + vectors[i]][squareCol + vectors[j]] === "1"
+        ) {
+          neighboursCount += 1;
+        }
+      }
+    }
+    return neighboursCount;
+  };
+  console.log(countSquareNeighbours([1, 1]));
   return (
     <>
       <div className="grid-board">{renderGrid()}</div>
--- src/components/GridSquare.jsx
+++ src/components/GridSquare.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 function GridSquare(props) {
   const classes = `grid-square color-${props.color}`;
-  return <div className={classes}></div>;
+  return <div className={classes} onClick={props.onClick}></div>;
 }
 
 GridSquare.propTypes = {
   color: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
 export default GridSquare;
